7412c307d20b7c6ba262285963e8a9f3b1861522
JuYeong <happyu9986@gmail.com>
2021-10-29 19:21:13 +0900

    [3-2][Firmware]CDS 광센서로 밝기에 따라 LED출력 다르게 하기 - millis함수로 변경

--- Firmware/CDS/CDS.ino
+++ Firmware/CDS/CDS.ino
@@ -1,22 +1,22 @@
-#include <SimpleTimer.h>
 #define CDS 0
 #define LED 5
 
-SimpleTimer Timer;
+unsigned long previousMillis = 0; // will store last time LED was updated
+const long interval = 1000; // interval at which to blink (milliseconds)
+int time = 0;
 
 void setup() {
   Serial.begin(9600);
   pinMode(LED, OUTPUT);
-  Timer.setInterval(1000);
 }
 
 void loop() {
-  
-  if(Timer.isReady()){
-    Serial.println("Called  every 1 sec");
+  unsigned long currentMillis = millis();
+  if(currentMillis - previousMillis >= interval){
+    previousMillis = currentMillis; // save the last time you blinked the LED
+    Serial.print(time++); Serial.println("초");
     int val = analogRead(CDS);
     Serial.println(255-(val/4));
     analogWrite(LED, 255-(val/4));
-    Timer.reset();
   }
 }
