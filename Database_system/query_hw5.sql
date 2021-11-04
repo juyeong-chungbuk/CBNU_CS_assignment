8dd81fcad47467397090ea1f9c3f1c7e91d50b71
JuYeong <happyu9986@gmail.com>
2021-11-04 15:18:42 +0900

    [3-2][DB]world db에서 데이터 출력

--- Database_system/query_hw5.sql
+++ Database_system/query_hw5.sql
@@ -1,7 +1,16 @@
 USE world;
 
-SELECT Name, Population FROM country WHERE Name='South Korea';
+SELECT Name, Population 
+FROM country 
+WHERE Name='South Korea';
 
-SELECT Name, Population FROM country WHERE Continent='Asia' ORDER BY Population DESC LIMIT 12;
+SELECT Name, Population 
+FROM country 
+WHERE Continent='Asia' 
+ORDER BY Population DESC 
+LIMIT 1;
 
-SELECT Continent, COUNT(Name) AS cnt_countries FROM country GROUP BY Continent HAVING COUNT(Name) >= 5;
+SELECT Continent, COUNT(Name) AS cnt_countries 
+FROM country 
+GROUP BY Continent 
+HAVING COUNT(Name) >= 5;
