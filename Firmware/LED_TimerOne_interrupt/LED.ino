63dfe676bce4b28e0b451de4e7a5e55ca6659b84
JuYeong <happyu9986@gmail.com>
2021-11-05 22:55:33 +0900

    [3-2][Firmware]TimerOne라이브러리 inetrrupt을 사용해 led 끄고 켜기

--- Firmware/LED_TimerOne_interrupt/LED.ino
+++ Firmware/LED_TimerOne_interrupt/LED.ino
@@ -1,15 +1,7 @@
 #include <TimerOne.h>
-#include "TM1637Display.h"
-
-#define CLK 10
-#define DIO 11
 #define LED 2
-
-TM1637Display fnd(CLK, DIO);
 volatile uint8_t tSec = 0;
-volatile uint8_t tMin = 0;
-volatile uint8_t tFlag = 1;
-volatile long tPeriod = 1000000;
+volatile long tPeriod = 100000;
 int led = 1;
 
 void setup(){
@@ -17,27 +9,22 @@ void setup(){
   pinMode(LED, OUTPUT);
   Timer1.initialize(tPeriod); // 1sec
   Timer1.attachInterrupt(OneSec);  // ISR
-  fnd.setBrightness(1);
-  fnd.showNumberDecEx(0, 0x40, true);
 }
 
 void OneSec()
 {
   tSec = tSec + 1;
   if(tSec == 3){
-    led = 0; 
+    led = 0;
   }
-  if(tSec == 10){
+  else if(tSec == 10){
     led = 1;
     tSec = 0;
-    tMin += 10;
   }
+
 }
 
 void loop(){
-  uint16_t num;
-  num = tMin + tSec;
-  fnd.showNumberDecEx(num);
   if(led == 1){   
     digitalWrite(LED, HIGH);
   }
