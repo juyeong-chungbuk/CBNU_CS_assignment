66d3575736bb6d95e3a61f5ca38e806295920a14
JuYeong <happyu9986@gmail.com>
2021-11-06 16:55:00 +0900

    [3-2][Firmware]TimerOne라이브러리 PWM을 사용해 led 끄고 켜기

--- Firmware/LED_TimerOne_PWM/LED_TimerOne_PWM.ino
+++ Firmware/LED_TimerOne_PWM/LED_TimerOne_PWM.ino
@@ -1,48 +1,13 @@
-//#include <TimerOne.h>
-//#define LED 9
-//int led = 0;
-//
-//void setup(){
-//  pinMode(LED, OUTPUT);
-//  Timer1.initialize(3000000); // 1sec
-//  Timer1.pwm(LED, 512);
-//}
-//
-//void loop(){
-//  if(led == 0){
-//    Timer1.setPeriod(3000000);
-//    Timer1.setPwmDuty(LED, 512);
-//    led = 1;
-//  }
-//  else{
-//    Timer1.setPeriod(7000000);
-//    Timer1.setPwmDuty(LED, 0);
-//    led = 0;
-//  }
-////  Timer1.pwm(LED, 1023, 3000000);
-////  Timer1.setPwmDuty(LED, 1023);
-////  Timer1.pwm(LED, 0, 7000000);
-////  Timer1.setPwmDuty(LED, 0);
-//}
-
 #include <TimerOne.h>
-#define LED 9
 
-volatile uint8_t tMsec = 0;
-volatile long tPeriod = 100000;
+#define LED 9
+volatile long tPeriod = 1000000;
 
 void setup(){
   pinMode(LED, OUTPUT);
-  Timer1.initialize(tPeriod); // 0.1초
+  Timer1.initialize(tPeriod); // 1초
+  Timer1.pwm(LED, 307);
 }
 
 void loop(){
-  if(tMsec == 3){
-    Timer1.pwm(LED, 1023);
-  }
-  if(tMsec == 10){
-    Timer1.pwm(LED, 0);
-    tMsec = 0;
-  }
-  tMsec += 1;
 }
